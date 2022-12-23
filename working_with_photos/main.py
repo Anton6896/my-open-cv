9e3d0cb53dcb2bd4695ee084bc6eecc2cab13821
Anton.R <Anton-R@live.com>
2022-12-23 17:19:16 +0200

    basic read ok

--- working_with_photos/main.py
+++ working_with_photos/main.py
@@ -1,6 +1,19 @@
 import os
+import cv2 as cv
 
-photos_path = os.path.join(os.path.abspath('..'), 'resources', 'Photos')
+
+photos_path = os.path.join(
+    os.path.abspath(os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), os.pardir)),
+    'resources',
+    'Photos')
+
+
+def basic_read():
+    img1 = cv.imread(os.path.join(photos_path, 'cat_large.jpg'))
+    cv.imshow('cat', img1)
+    cv.waitKey(0)  # keyboard interaction
+
 
 if __name__ == '__main__':
-    ...
+    basic_read()
