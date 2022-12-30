6a3ecf277efbfc20f2e37c6688851265b5b480ad
anton <anton-r@live.com>
2022-12-30 18:25:58 +0200

    basic func

---
+++ basics/shapes.py
@@ -0,0 +1,50 @@
+import cv2 as cv
+import numpy as np
+
+# https://github.com/jasmcaus/opencv-course/blob/master/Section%20%231%20-%20Basics/basic_functions.py
+blank = np.zeros((500, 500, 3), dtype='uint8')
+
+
+def basic():
+    cv.imshow('Blank', blank)
+
+    """
+    - put color to shape [y, x] and color in BGR
+    [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+    ]
+    """
+    blank[100:200, 200:400] = 0, 0, 255
+    cv.imshow('RedSmall', blank)
+
+
+def basic_2():
+    blank = np.zeros((500, 500, 3), dtype='uint8')
+    cv.imshow('Blank', blank)
+
+    # 1. Paint the image a certain colour
+    blank[200:300, 300:400] = 0, 0, 255
+    cv.imshow('Green', blank)
+
+    # 2. Draw a Rectangle
+    cv.rectangle(blank, (0, 0), (blank.shape[1] // 2, blank.shape[0] // 2), (0, 255, 0), thickness=-1)
+    cv.imshow('Rectangle', blank)
+
+    # 3. Draw A circle
+    cv.circle(blank, (blank.shape[1] // 2, blank.shape[0] // 2), 40, (0, 0, 255), thickness=-1)
+    cv.imshow('Circle', blank)
+
+    # 4. Draw a line
+    cv.line(blank, (100, 250), (300, 400), (255, 255, 255), thickness=3)
+    cv.imshow('Line', blank)
+
+    # 5. Write text
+    cv.putText(blank, 'Hello, my name is Jason!!!', (0, 225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0, 255, 0), 2)
+    cv.imshow('Text', blank)
+
+
+def use_shapes():
+    basic()
+    cv.waitKey(0)
